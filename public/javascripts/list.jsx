d158690854ba01341ab04216ad9e5a0355f69d53
x42 <k421m41@gmail.com>
2017-02-19 17:58:11 +0900

    modified

--- public/javascripts/list.jsx
+++ public/javascripts/list.jsx
@@ -4,8 +4,8 @@ export default class List extends React.Component {
     render() {
         return (
             <ul> {
-                this.props.data.map((a, i) =>{
-                  return <li>{ a }</li>
+                this.props.data.map((val) =>{
+                  return <li>{ val }</li>
                 })
               }
             </ul>
